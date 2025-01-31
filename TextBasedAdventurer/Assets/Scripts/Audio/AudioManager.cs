bf32cafd96afe71bf10cb4c70929c28f51640f01
GLLRMGNZL <guillegonza95@gmail.com>
2025-01-31 14:00:24 +0100

    Add audio system
    Added audio system + placeholders for music and typewriter effects

---
+++ TextBasedAdventurer/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.spatialBlend = 0f;
+            Debug.Log(s.name);
+        }
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("The sound " + name + " was not found!");
+            return;
+        }
+        else
+        {
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.Play();
+        }
+    }
+}
