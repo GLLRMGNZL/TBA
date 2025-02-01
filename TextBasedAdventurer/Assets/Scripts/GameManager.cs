d0734a2a98831fdc08b2074a146222cf6ce1d309
GLLRMGNZL <guillegonza95@gmail.com>
2025-02-01 13:35:56 +0100

    Add full screen toggle
    - Add full screen toggle in options menu

--- TextBasedAdventurer/Assets/Scripts/GameManager.cs
+++ TextBasedAdventurer/Assets/Scripts/GameManager.cs
@@ -33,4 +33,10 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    // Full Screen
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
 }
